f42e0810694f6f22649f71d235494807ab6ad8a9
Aman <amantiwari8861@gmail.com>
2021-07-25 16:53:12 +0530

    arithematic operations done

--- add.c
+++ add.c
@@ -2,11 +2,14 @@
 int main()
 {
 	int num,num2,result;
-	printf("enter the value in num1 ");
-	scanf("%d",&num);
-	printf("enter the value in num2 ");
-	scanf("%d",&num2);
-	result=num+num2;
+//	printf("enter the value in num1 ");
+//	scanf("%d",&num);
+//	printf("enter the value in num2 ");
+//	scanf("%d",&num2);
+	printf("enter the value in \n num and num1 :\n");
+	scanf("%d %d",&num,&num2);
+//	result=num+num2;
+	result=num%num2;
 	printf("the sum of %d and %d is %d ",num,num2,result);
 	return 0;
 }
