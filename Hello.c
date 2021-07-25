9918ed2895b649ce004d5b5c98e990ec06b5ee4f
Aman <amantiwari8861@gmail.com>
2021-07-25 16:26:52 +0530

    c batch new

---
+++ Hello.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()  // starting point of a program
+{ //body starting of main
+	printf("Hello World ! "); //to print message on output screen
+	getch(); // to hold the output screen
+	return 0; // ending point of program
+	printf("My name is Aman "); 
+} //body finished of main
+//semicolon(;) is used to terminate the line 
+// single line Comment (use this if u want to ignore something in ur program)
+/*
+multiple
+line 
+comment 
+*/
---
+++ add.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int num,num2,result;
+	printf("enter the value in num1 ");
+	scanf("%d",&num);
+	printf("enter the value in num2 ");
+	scanf("%d",&num2);
+	result=num+num2;
+	printf("the sum of %d and %d is %d ",num,num2,result);
+	return 0;
+}
